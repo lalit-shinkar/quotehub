800e5533148313f888d56a22c950637fe6d4f377
lalit-shinkar <shinkarlalit18@gmail.com>
2025-05-13 20:30:55 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,21 @@
-from flask import Flask
+# from flask import Flask
+# from app.api.routes import quote_api
+
+# def create_app():
+#     app = Flask(__name__)
+#     app.register_blueprint(quote_api)
+#     return app
+
+from flask import Flask, render_template
 from app.api.routes import quote_api
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(quote_api)
-    return app
 
+    # Route for frontend
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    return app
